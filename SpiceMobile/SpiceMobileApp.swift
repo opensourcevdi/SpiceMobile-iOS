ed557afbb541efeb9d07dd8648f37a310fb98345
Lennard Siegel <lenny.siegel@icloud.com>
2025-12-01 18:02:07 +0100

    Initial Commit

---
+++ SpiceMobile/SpiceMobileApp.swift
@@ -0,0 +1,21 @@
+//
+//  SpiceMobileApp.swift
+//  SpiceMobile
+//
+//  Created by Lennard Siegel on 01.12.25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct SpiceMobileApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
